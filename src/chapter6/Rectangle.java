280727125e3ac179b3c0a3730095732704fe38b4
Nazarej, Matus <matus.nazarej@accenture.com>
2023-03-26 18:58:11 +0200

    [ADD] chapter6

--- src/chapter6/Rectangle.java
+++ src/chapter6/Rectangle.java
@@ -1,5 +1,40 @@
 package chapter6;
 
 public class Rectangle {
+    private double length;
+    private double width;
 
+    public Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    public Rectangle(double length, double width){
+        this.length = length; //Can be set this way
+        setWidth(width); //or Can be set in this way, these two are here to demonstrate options
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public void setLength(double length){
+        this.length = length;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    double calucateParimeter(){
+        return (2*length)+(2*width);
+    }
+
+    double calculateArea(){
+        return length*width;
+    }
 }
